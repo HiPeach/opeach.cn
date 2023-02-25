fe5d86254d9bfdaa19eac0b5a25623ecfc0d13ff
HiPeach <hi@hipeach.eu.org>
2023-02-25 19:52:10 +0800

    Site updated: 2023-02-25 19:52:09

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,20 @@
+// 获取元素
+var myElement = document.getElementById("my-element");
+
+// 监听窗口滚动事件
+window.addEventListener("scroll", function() {
+  // 获取当前滚动位置
+  var scrollPosition = window.scrollY || window.pageYOffset || document.body.scrollTop || document.documentElement.scrollTop;
+  
+  // 如果滚动位置为0，则页面在最顶部，更改元素的背景颜色
+  if (scrollPosition === 0) {
+    document.querySelector('#nav').style.backdropFilter = 'none';
+  } else {
+    // 否则，页面不在最顶部，将元素的背景颜色设置为默认值
+    document.querySelector('#nav').style.backdropFilter = 'saturate(180%) blur(20px)';
+  }
+});
+
 // 右下角悬浮菜单栏 - 分享 - https://blog.leonus.cn/2022/rightside.html
 function share() {
     let url = window.location.origin + window.location.pathname
@@ -291,3 +308,25 @@ setInterval(() => {
     currentTimeHtml += time[1] + ' DAYS ' + time[2] + ' : ' + time[3] + ' : ' + time[4];
     document.getElementById("runtime").innerHTML = currentTimeHtml;
 }, 1000);
+
+// 返回顶部 显示网页阅读进度
+window.onscroll = percent; // 执行函数
+// 页面百分比
+function percent() {
+  let a = document.documentElement.scrollTop || window.pageYOffset, // 卷去高度
+    b =
+      Math.max(
+        document.body.scrollHeight,
+        document.documentElement.scrollHeight,
+        document.body.offsetHeight,
+        document.documentElement.offsetHeight,
+        document.body.clientHeight,
+        document.documentElement.clientHeight
+      ) - document.documentElement.clientHeight, // 整个网页高度 减去 可视高度
+    result = Math.round((a / b) * 100), // 计算百分比
+    btn = document.querySelector("#percent"); // 获取图标
+
+  result <= 99 || (result = 99), (btn.innerHTML = result);
+}
+
+document.getElementById("page-name").innerText = document.title.split(" | 安知鱼")[0];
